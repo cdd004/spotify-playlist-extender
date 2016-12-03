da918a4527bf783a03d901006a6afd20da0f5466
ninjadood7 <josephcho10@yahoo.com>
2016-12-03 00:35:38 -0500

    Reworked search result UI, still lots to fix

--- project/authorization_code/public/js/controllers/searchController.js
+++ project/authorization_code/public/js/controllers/searchController.js
@@ -1,4 +1,12 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', [])
+    .config( [
+    '$compileProvider',
+    function( $compileProvider )
+    {   
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|spotify):/);
+        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    }
+]);
 
 //-------------------------------------------
 // var express = require('express');
@@ -8,18 +16,32 @@ var app = angular.module('myApp', []);
 // mongoose.connect('mongodb://localhost/spotify-test');
 //-------------------------------------------
 
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function($scope, $http, $compile) {
 
     $scope.userInput = "";
     $scope.response_unpack = "";
     //$scope.toggle = {switch:true};
     $scope.toggle = true;
     $scope.start = true;
+    $scope.similar_response = 1;
+    $scope.artists = [];
+    $scope.tracks = [];
 
     $scope.toggleStart = function() {
         $scope.start = false;
         console.log('start has been toggled!');
         console.log("$scope.start = " + $scope.start);
+    }
+
+    $scope.getSimilar = function(track, name) {
+        console.log('calling getSimilar()');
+        $http.get('http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist='+name+'&track='+track+'&api_key=&format=json')
+        .then(function(response) {
+
+            $scope.similar_response = response.data.similartracks.track[0];
+            alert("Similar track: " + $scope.similar_response.name + " by " + $scope.similar_response.artist.name);
+        });
+        console.log('finished getSimilar()');
     }
 
     $scope.clearSearch = function() {
@@ -38,15 +60,15 @@ app.controller('myCtrl', function($scope, $http) {
 
     		console.log(response);
     		//var.data.artists.items[0].name
-    		var artists = response.data.artists.items;
-    		// console.log(artists);
-    		// console.log(artists[0].name)
+    		//var artists = response.data.artists.items;
+            $scope.artists = response.data.artists.items;
 
-    		var tracks = response.data.tracks.items;
-    		console.log(tracks[0].name)
+    		//var tracks = response.data.tracks.items;
+            $scope.tracks = response.data.tracks.items;
+    		//console.log(tracks[0].name)
 
     		//clear the previous result
-    		document.getElementsByClassName('jsonResponse')[0].innerHTML = null;
+    		document.getElementsByClassName('jsonResponse').innerHTML = null;
 
             var scb = function() {
                 console.log('success');
@@ -88,30 +110,32 @@ app.controller('myCtrl', function($scope, $http) {
             //--------------------------
 
             //display all the artists 
-    		for (i = 0; i < artists.length; i++) {
-                if (i == 0) {
-                    document.getElementsByClassName('jsonResponse')[0].innerHTML += '<h2>Artists</h2>';
-                }
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '<div class="artistsResponse">';
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '<p> Artist: ' + artists[i].name + '</p>';
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a href='+artists[i].uri+'>Open in Spotify</a> <br>';
-                document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a target="_blank" href='+artists[i].external_urls.spotify+'>Open in browser</a>';
-                document.getElementsByClassName('jsonResponse')[0].innerHTML += '<br>';
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '</div>';
-    		}
+    		// for (i = 0; i < artists.length; i++) {
+      //           if (i == 0) {
+      //               document.getElementsByClassName('jsonResponse')[0].innerHTML += '<h2>Artists</h2>';
+      //           }
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '<div class="artistsResponse">';
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '<p> Artist: ' + artists[i].name + '</p>';
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a href='+artists[i].uri+'> <img src="../../../images/spotify-link.png" width="25px" height="25px"> </a>';
+      //           document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a target="_blank" href='+artists[i].external_urls.spotify+'> <img src="../../../images/browser-link.png" width="25px" height="25px"> </a>';
+      //           document.getElementsByClassName('jsonResponse')[0].innerHTML += '<br>';
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '</div>';
+    		// }
 
             //display all the tracks & their respective artists
-    		for (i = 0; i < tracks.length; i++) {
-                if (i == 0) {
-                    document.getElementsByClassName('jsonResponse')[0].innerHTML += '<h2>Tracks</h2>';
-                }
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '<div>';
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '<p> Track: ' + tracks[i].name + ' by ' + tracks[i].artists[0].name + '</p>';
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a href='+tracks[i].uri+'>Open in Spotify</a> <br>';
-                document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a target="_blank" href='+tracks[i].external_urls.spotify+'>Open in browser</a>';
-                document.getElementsByClassName('jsonResponse')[0].innerHTML += '<br>';
-    			document.getElementsByClassName('jsonResponse')[0].innerHTML += '</div>';
-    		}
+    		// for (i = 0; i < tracks.length; i++) {
+      //           if (i == 0) {
+      //               document.getElementsByClassName('jsonResponse')[0].innerHTML += '<h2>Tracks</h2>';
+      //           }
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '<div>';
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '<p> Track: ' + tracks[i].name + ' by ' + tracks[i].artists[0].name + '</p>';
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a href='+tracks[i].uri+'> <img src="../../../images/spotify-link.png" width="25px" height="25px"> </a>';
+      //           document.getElementsByClassName('jsonResponse')[0].innerHTML += '<a target="_blank" href='+tracks[i].external_urls.spotify+'> <img src="../../../images/browser-link.png" width="25px" height="25px"> </a>';
+      //           document.getElementsByClassName('jsonResponse')[0].innerHTML += '<button ng-click="getSimilar()"> <img src="../../../images/similar-link.png" width="25px" height="25px"> </button>';
+      //           document.getElementsByClassName('jsonResponse')[0].innerHTML += '<br>';
+    		// 	document.getElementsByClassName('jsonResponse')[0].innerHTML += '</div>';
+
+    		// }
     	});
     }
 });
